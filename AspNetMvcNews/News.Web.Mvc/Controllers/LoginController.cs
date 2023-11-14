343c2856d510f6199225c7ca4c44f5b46fca37f2
tuggcebolat <138963442+tuggcebolat@users.noreply.github.com>
2023-11-14 16:06:51 +0300

    14/11/2023

---
+++ AspNetMvcNews/News.Web.Mvc/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace News.Web.Mvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
