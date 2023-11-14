8dc9f0ce14dfb8b403de522b1380a01f905cf41a
tuggcebolat <138963442+tuggcebolat@users.noreply.github.com>
2023-11-14 12:50:09 +0300

    Created Template

---
+++ AspNetMvcNews/News.Web.Mvc/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace News.Web.Mvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcNews/News.Web.Mvc/Controllers/SinglePostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace News.Web.Mvc.Controllers
+{
+    public class SinglePostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
