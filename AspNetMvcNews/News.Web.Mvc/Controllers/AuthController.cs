14ffab88afbea905862238017b21fdf87a0e9776
tuggcebolat <138963442+tuggcebolat@users.noreply.github.com>
2023-11-15 09:35:51 +0300

    15/11/2023

---
+++ AspNetMvcNews/News.Web.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News.Entities.Models;
+
+namespace News.Web.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (!string.IsNullOrEmpty(user.Email))
+            {
+                return RedirectToAction("Index","Category");
+            }
+            return RedirectToAction("Register","Auth");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+      
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
